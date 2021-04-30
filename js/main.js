81b08e2a9b7fd70b999cf67f2534f279cf592a3f
juliaross <33494402+juliaross@users.noreply.github.com>
2021-04-30 18:20:39 +0300

    new main page

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 $(function(){
 
-  $('.wrapper').fadeIn(1000);
-  $('.main_title').addClass('active');
-  $('.main_description').addClass('active');
-  $('.main_arrow').addClass('active');
+  $('.wrapper').fadeIn(500);
+  // $('.main_title').addClass('active');
+  // $('.main_description').addClass('active');
+  // $('.main_arrow').addClass('active');
+
+
+
+  $('.open-contact').on('click', function(event){
+  // event.preventDefault();
+  $('.main').addClass('move-out');
+  $('.main-nav').addClass('is-visible');
+});
+//close menu
+$('.cd-close-menu').on('click', function(event){
+  event.preventDefault();
+  $('.main').removeClass('move-out');
+  $('.main-nav').removeClass('is-visible');
+});
+
 
 });
