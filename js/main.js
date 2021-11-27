31f626bc9cdcd95472833472a2b8f66cae8aa0a3
juliaross <juliana.roshchina@gmail.com>
2021-11-27 22:17:03 +0300

    new main page

--- js/main.js
+++ js/main.js
@@ -1,23 +1,47 @@
-$(function(){
+"use strict";
 
-  $('.wrapper').fadeIn(500);
-  // $('.main_title').addClass('active');
-  // $('.main_description').addClass('active');
-  // $('.main_arrow').addClass('active');
-
-
-
-  $('.open-contact').on('click', function(event){
-  // event.preventDefault();
-  $('.main').addClass('move-out');
-  $('.main-nav').addClass('is-visible');
+gsap.registerPlugin(ScrollTrigger);
+var locoScroll = new LocomotiveScroll({
+  el: document.querySelector('[data-scroll-container]'),
+  smooth: true
 });
-//close menu
-$('.cd-close-menu').on('click', function(event){
-  event.preventDefault();
-  $('.main').removeClass('move-out');
-  $('.main-nav').removeClass('is-visible');
+locoScroll.on("scroll", ScrollTrigger.update);
+ScrollTrigger.scrollerProxy('[data-scroll-container]', {
+  scrollTop: function scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  },
+  getBoundingClientRect: function getBoundingClientRect() {
+    return {
+      top: 0,
+      left: 0,
+      width: window.innerWidth,
+      height: window.innerHeight
+    };
+  },
+  pinType: document.querySelector('[data-scroll-container]').style.transform ? "transform" : "fixed"
 });
+var works = document.querySelectorAll('.work');
 
+for (var i = 0; i < works.length; i++) {
+  works[i].classList.add("work".concat(i));
+  gsap.from(".work".concat(i), {
+    scrollTrigger: {
+      trigger: ".work".concat(i),
+      scroller: "[data-scroll-container]",
+      scrub: true,
+      start: "top bottom",
+      end: "top center" // onUpdate() {
+      //   console.log("Update")
+      // }
 
+    },
+    opacity: 0,
+    y: 100,
+    ease: Power2["in"]
+  });
+}
+
+ScrollTrigger.addEventListener("refresh", function () {
+  return locoScroll.update();
 });
+ScrollTrigger.refresh();
